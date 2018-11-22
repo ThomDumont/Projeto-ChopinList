d40c083b4e978ca5c5a92d16e5d60d03772765f8
Thomas Dumont das Neves <tdn.1999@gmail.com>
2018-11-22 19:39:38 -0200

    criando os btn e edit text do anunciar

--- appChopinsList/app/src/main/java/app/chopinslist/AnunciarActivity.java
+++ appChopinsList/app/src/main/java/app/chopinslist/AnunciarActivity.java
@@ -2,12 +2,24 @@ package app.chopinslist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AnunciarActivity extends AppCompatActivity {
+
+    EditText txt_titulo, txt_descricao;
+    Button btnAnunc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anunciar);
+
+        txt_titulo = (EditText) findViewById(R.id.txt_titulo);
+        txt_descricao = (EditText) findViewById(R.id.txt_descricao);
+        btnAnunc = (Button) findViewById(R.id.btnAnunc);
+        
     }
+
+
 }
