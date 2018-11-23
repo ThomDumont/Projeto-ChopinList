857d5b16e568f688e14caab42b57d42d086b1255
Pietro Montoro Rigetto <pietro.montoro@gmail.com>
2018-11-23 19:00:49 -0200

    tudo completinho

--- appChopinsList/app/src/main/java/app/chopinslist/PerfilActivity.java
+++ appChopinsList/app/src/main/java/app/chopinslist/PerfilActivity.java
@@ -2,12 +2,39 @@ package app.chopinslist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import app.chopinslist.Models.User;
+import app.chopinslist.helper.dbHelper;
 
 public class PerfilActivity extends AppCompatActivity {
+
+    TextView login, senha, nome, tele, email;
+    User u;
+
+    dbHelper db = new dbHelper(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
+
+        login = findViewById(R.id.pLogin);
+        senha = findViewById(R.id.pSenha);
+        nome = findViewById(R.id.pNome);
+        tele = findViewById(R.id.pTelefone);
+        email = findViewById(R.id.pEmail);
+        u = (User) getIntent().getSerializableExtra("usuario");
+
+        User w = db.buscaUsuario(u);
+
+        login.setText(w.getLogin());
+        senha.setText(w.getSenha());
+        nome.setText(w.getNome());
+        tele.setText(w.getTelefone());
+        email.setText(w.getEmail());
+
+
     }
 }
